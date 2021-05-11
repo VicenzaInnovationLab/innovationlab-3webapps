85c09f5422c57305e22df3d584dc2b93b3f7a0af
Yaroslav Vasyunin <y.vasyunin@icloud.com>
2021-05-11 11:15:45 +0200

    enhance vector processing, change output field name and remove duplicate columns

--- processing/vector_prep.py
+++ processing/vector_prep.py
@@ -4,17 +4,8 @@ import numpy as np
 import geopandas as gpd
 import pandas as pd
 import rasterio
-from rasterstats import zonal_stats
 
-
-def get_zonal_stats(vector, raster, stats, nodata, affine_):
-    """Esegue le statistiche zonali e salva il risultato in Geopandas DataFrame"""
-    result = zonal_stats(vector, raster,
-                         nodata=nodata, stats=stats,
-                         affine=affine_, geojson_out=True)
-    geostats = gpd.GeoDataFrame.from_features(result, crs=vector.crs)
-    return geostats
-
+from main import get_zonal_stats
 
 # confini comunali Istat
 com_file = Path.cwd() / ".." / "data" / "input" / "istat_comuni2021.zip"
@@ -22,19 +13,17 @@ com = gpd.read_file(com_file)
 com.columns = map(str.lower, com.columns)
 com = com[["comune", "pro_com", "cod_prov", "cod_reg", "geometry"]]
 
-
 # statistiche zonali per riempire la popolazione
 worldpop_file = Path.cwd() / ".." / "data" / "input" / "worldpop.tif"
 worldpop_ = rasterio.open(worldpop_file)
 affine = worldpop_.transform
 worldpop = worldpop_.read(1)
 
-print("calcolo delle statistiche...")
+print("preparazione del dataset con i confini comunali...")
 
 com = get_zonal_stats(com, worldpop, ["sum"], np.nan, affine)
 com.rename(columns={"sum": "pop"}, inplace=True)
 com["pop"] = com["pop"].fillna(0).astype(int)
-
 
 # censimento della popolazione Istat
 pop_file = Path.cwd() / ".." / "data" / "input" / "istat_pop2019.csv"
@@ -46,7 +35,6 @@ pop.rename(columns={"itter107": "pro_com",
                     "value": "pop"
                     },
            inplace=True)
-
 
 # dataset unito
 com_pop = com.join(pop.set_index("pro_com"),
@@ -60,12 +48,12 @@ com_pop["pop"] = np.where(com_pop["pop_cens"].isnull,
                           com_pop["pop_cens"],
                           com_pop["pop_com"])
 
-
 # aggiungere nomi di provincie e regioni
 prov_file = Path.cwd() / ".." / "data" / "input" / "istat_codici_prov.csv"
 prov = pd.read_csv(prov_file, sep=";", na_filter=False)
 prov.columns = map(str.lower, prov.columns)
-prov = prov[["cod_reg", "den_reg", "cod_prov_storico", "den_uts", "sigla automobilistica"]]
+prov = prov[["cod_reg", "den_reg", "cod_prov_storico",
+             "den_uts", "sigla automobilistica"]]
 prov.rename(columns={"sigla automobilistica": "sigla",
                      "cod_prov_storico": "cod_prov",
                      "den_uts": "den_prov"}, inplace=True)
@@ -78,10 +66,15 @@ com_pop_pr["cod_com"] = com_pop_pr.index
 com_pop_pr.rename(columns={"comune_com": "den_com",
                            "pro_com": "cod_com"},
                   inplace=True)
-com_f = com_pop_pr[["cod_com", "den_com", "sigla", "cod_prov",
-                    "den_prov", "cod_reg", "den_reg", "pop", "geometry"]]
-
+com_geog = com_pop_pr[["den_com", "sigla", "cod_prov",
+                       "den_prov", "cod_reg", "den_reg", "pop", "geometry"]]
+com_geog.index.names = ["cod_com"]
+com_geog["pop"].fillna(-1, inplace=True)
+com_geog["pop"] = com_geog["pop"].astype(int)
+com_f_nogeog = com_geog.loc[:, com_geog.columns != "geometry"]
 
 # salvare il file
-com_f_file = Path.cwd() / ".." / "data" / "output" / "istat_com_pop"
-com_f.to_file(com_f_file, driver="ESRI Shapefile")  # oppure GeoJSON
+com_geog_f = Path.cwd() / ".." / "data" / "output" / "istat_com_pop"
+com_geog.to_file(com_geog_f, driver="ESRI Shapefile")
+
+com_f_nogeog.to_csv(f"{com_geog_f}.csv")
